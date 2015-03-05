d68cba655cbc78763f414d1b083ec63c563078df
Zack Fischmann <zacharyrobert@gmail.com>
2015-03-05 13:57:00 -0800

    "finished stringifyJSON"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,47 +3,45 @@
 
 // but you don't so you're going to write it from scratch:
 
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj){
     var helperFunc = function(obj){
         if(Array.isArray(obj)){
-            console.log(obj, "hit array..."); 
-            if (obj.length === 0){
-                return '[]';
-            }
-            else {
-                return '[' + helperFunc(obj[0]) + ']';
-            }
-        }
-        else if(Object.prototype.toString.call(obj) === "[object Array]"){
-            console.log(obj, 'hit an object...');
-            return;
+            return '[' + obj.map(helperFunc) + ']';
         }
         else if(typeof(obj) === "number" | typeof(obj) === "boolean"){
-            console.log(obj, 'hit boolean or number...')
             return obj.toString();
         }
         else if(obj === null){
-            console.log(obj, "hit null...")
             return "null";
         }
         else if(typeof(obj) === "string"){
-            console.log(obj, 'hit string...')
             return '"' + obj.toString() + '"';
         }
+        else if(typeof(obj) === "object"){
+            function objHelper(obj){
+                var str = '';
+                var len = Object.keys(obj).length;
+                for (key in obj){
+                        if(!(typeof(obj[key]) === "function" | typeof(obj[key]) === "undefined")){
+                            if (len > 1){
+                                str += helperFunc(key) + ":" + helperFunc(obj[key]) + ',';
+                            }
+                            else {
+                                str += helperFunc(key) + ":" + helperFunc(obj[key]);
+                            }
+                        }
+                len = len-1;
+                }
+                return str;
+            }
+            return  '{' + objHelper(obj) + '}';
+        }
         else{
-            console.log(obj, 'hit other...');
             return '';
         }
     };
     var result = helperFunc(obj);
-    console.log(result);
     return result;
-
 };
 
 
