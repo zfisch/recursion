8ffedaaeec825e50b69f4397974bbaf72dd82206
Zack Fischmann <zacharyrobert@gmail.com>
2015-03-05 14:43:56 -0800

    "implemented getElementsByClassName"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,15 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+    var elements = [];
+    var helperFunc = function(html){
+        console.log(html);
+        if(_.contains(html.classList, className)){
+            elements.push(html);
+        };
+        _.each(html.childNodes, function(node){ helperFunc(node) });
+    };
+    helperFunc(document.body);
+    return elements;
 };
